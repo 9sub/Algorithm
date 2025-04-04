a0bd434b38392ed600fb22ef2b51b98e18567502
이규섭 <guseob0701@naver.com>
2025-04-04 22:43:24 +0900

    [level 1] Title: 체육복, Time: 0.02 ms, Memory: 9.25 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -0,0 +1,11 @@
+def solution(n, lost, reserve):
+    cnt=0
+    _lost = [l for l in lost if l not in reserve]
+    _reserve = [r for r in reserve if r not in lost]
+    _lost.sort(), _reserve.sort()
+    for r in _reserve:
+        if r-1 in _lost:
+            _lost.remove(r-1)
+        elif r+1 in _lost:
+            _lost.remove(r+1)
+    return n-len(_lost)

c24306e6c947320fbe88d1de28e6ce92b7964aea
이규섭 <guseob0701@naver.com>
2025-04-23 21:02:12 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 3249.25 ms, Memory: 9.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.py
@@ -0,0 +1,8 @@
+def solution(s):
+    answer = 0
+    for i in range(len(s)):
+        for j in range(i+1, len(s)+1):
+            if s[i:j]==s[i:j][::-1]:
+                answer = max(len(s[i:j]), answer)
+
+    return answer

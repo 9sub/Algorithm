ab8a8b7861ca4971cb9665947726010c1d64d72a
이규섭 <guseob0701@naver.com>
2025-05-16 14:18:08 +0900

    [level 1] Title: 과일 장수, Time: 152.70 ms, Memory: 20.6 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/README.md
+++ 프로그래머스/1/135808. 과일 장수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 과일 장수 - 135808 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/135808) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/135808?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.01 ms
+메모리: 20.6 MB, 시간: 152.70 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 03일 15:06:16
+2025년 05월 16일 14:18:06
 
 ### 문제 설명
 
--- 프로그래머스/1/135808. 과일 장수/과일 장수.py
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.py
@@ -1,12 +1,10 @@
 def solution(k, m, score):
     score.sort(reverse=True)
-    i = 0
-    result = 0
-    while i<len(score):
-        if i+m > len(score):
-            break
-        a = score[i:i+m]
-        minn = min(a)
-        result += minn*m
-        i+=m
-    return result
+    tmp= 0
+    sum_=0
+    while tmp < len(score):
+        if len(score[tmp:tmp+m]) == m:
+            sum_ += min(score[tmp:tmp+m])*m
+        tmp += m
+    
+    return sum_

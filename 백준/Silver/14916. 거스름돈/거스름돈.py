ac817eebe6ac257cd65bc426c77c0bb0287950a1
이규섭 <guseob0701@naver.com>
2025-08-10 12:36:31 +0900

    [Silver V] Title: 거스름돈, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/14916. 거스름돈/거스름돈.py
@@ -0,0 +1,15 @@
+
+
+n = int(input())
+
+cnt = 0
+found = False
+for i in range(n//2 + 1):
+    if (n - 2*i) % 5 == 0:
+        print(i + (n-2*i)//5)
+        found = True
+        break
+
+if not found:
+    print(-1)
+

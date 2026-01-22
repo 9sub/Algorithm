3705a9249af125ca3d13e8fc5cedd85c9485f37f
이규섭 <guseob0701@naver.com>
2026-01-22 13:33:50 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 176 ms, Memory: 17820 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Main {
+	
+    public static void main(String[] args) {
+    	
+    	Scanner sc = new Scanner(System.in);
+    	int n = sc.nextInt();
+
+    	
+    	
+    	
+    	
+    	for(int i=1;i<=n;i++) {
+    		System.out.print(" ".repeat(n-i));
+    		System.out.println("*".repeat(2*i-1));
+    	}
+    	
+    	
+    	
+    	for(int i=n-1;i>0;i--) {
+    		System.out.print(" ".repeat(n-i));
+    		System.out.println("*".repeat(2*i-1));
+    		
+    	}
+    	
+    }
+}
+
+
+
+
+
+
+
+
+

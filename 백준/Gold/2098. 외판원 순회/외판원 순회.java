b2702b755d12c4dc399f76008e922a3c32a85b93
이규섭 <guseob0701@naver.com>
2026-03-03 11:47:54 +0900

    [Gold I] Title: 외판원 순회, Time: 144 ms, Memory: 15604 KB -BaekjoonHub

--- 백준/Gold/2098. 외판원 순회/외판원 순회.java
+++ 백준/Gold/2098. 외판원 순회/외판원 순회.java
@@ -2,64 +2,53 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N;
-    static int Arange;  // 집합 A를 비트마스크로 표현했을 때 가질 수 있는 최대 값. 즉, 2^(N-1)
-    static int[][] W;  // 인접 행렬
-    static int[][] D;
-    static final int INF = Integer.MAX_VALUE;  // infinite 를 대변 
-
+    static int n;
+    static int arange; // 비트 마스크로 표현했을때 가질 수 있는 최대 값 2^n-1
+    static int[][] w;
+    static int[][] dist;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
 
-        N = Integer.parseInt(br.readLine());
-        W = new int[N][N];
+        w = new int[n][n];
 
-        for (int i = 0; i < N; ++i) {
+        for(int i = 0; i < n; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
-            
-            for (int j = 0 ; j < N; ++j)
-                W[i][j] = Integer.parseInt(st.nextToken());
+            for(int j = 0; j < n; j++){
+                w[i][j] = Integer.parseInt(st.nextToken());
+            }
         }
 
-        // tsp[i][A] 0번 도시에서 A에 속한 도시를 거친 후 i번 도시로 오는 최소 비용
-        Arange = (int) Math.pow(2, N - 1);
-        D = new int[N][Arange];
+        arange = (int)Math.pow(2, n-1);
+        dist = new int[n][arange];
 
-        // 구하지 않은 값은 모두 -1 로 초기화
-        for (int i = 0; i < N; ++i)
-            for (int j = 0; j < Arange; ++j)
-                D[i][j] = -1;
-
-        // 초기값 설정
-        D[0][0] = 0;
-        for (int i = 1; i < N; ++i) {
-            if (W[i][0] == 0) D[i][0] = Integer.MAX_VALUE;
-            else D[i][0] = W[i][0];
+        for(int i = 0; i < n; i++){
+            for(int j=0; j<arange; j++){
+                dist[i][j] = -1;
+            }
         }
 
-        System.out.println(tsp(0, Arange - 1));
+        dist[0][0] = 0;
+        for(int i=1;i<n;i++){
+            if(w[i][0] == 0) dist[i][0] = Integer.MAX_VALUE;
+            else dist[i][0] = w[i][0];
+        }
+        System.out.println(tsp(0,arange-1));
+
     }
 
-    public static int tsp(int i, int a) {
-        
-        // 이미 D[i][a] 값을 구했었다면, 해당 값을 반환
-        if (D[i][a] != -1)
-            return D[i][a];
+    public static int tsp(int i, int a){
+        if(dist[i][a] != -1) return dist[i][a];
 
-        int minCost = INF;
-        for (int j = 1; j < N; ++j) {
-            // 1번 도시부터 (N-1)번 도시 중 a 에 속한 도시만 고려한다.
-            if ((a & 1 << (j - 1)) == 0) continue;
-            
-            // prev = D[j][A - {j}]
-            int prev = tsp(j, a & ~(1 << (j - 1)));
-            
-            // 갈 수 없는 루트인 경우 건너뛴다.
-            if (prev == INF || W[i][j] == 0) continue;
-            
-            minCost = Math.min(minCost, W[i][j] + prev);
+        int min = Integer.MAX_VALUE;
+        for(int j=1;j<n;++j){
+            if((a & 1 << (j-1)) == 0 ) continue;
+
+            int prev = tsp(j, a & ~(1 << (j-1)));
+            if(prev == Integer.MAX_VALUE || w[i][j] == 0) continue;
+            min = Math.min(min, w[i][j]+prev);
         }
-        
-        return D[i][a] = minCost;
+        return dist[i][a]  = min;
     }
+
 }

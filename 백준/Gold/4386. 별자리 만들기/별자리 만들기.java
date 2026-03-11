a62d6630e8b98d9fd261d138af3811fdc5f86ee5
이규섭 <guseob0701@naver.com>
2026-03-11 10:25:57 +0900

    [Gold III] Title: 별자리 만들기, Time: 120 ms, Memory: 13216 KB -BaekjoonHub

--- 백준/Gold/4386. 별자리 만들기/별자리 만들기.java
+++ 백준/Gold/4386. 별자리 만들기/별자리 만들기.java
@@ -5,16 +5,15 @@ import java.util.*;
 public class Main {
 
     static int n,m,k;
-    static int[] parent;
+    static boolean[] visit;
     static int[] rank;
-    static ArrayList<Node> arr;
+    static ArrayList<Node>[] arr;
 
     static class Node implements Comparable<Node> {
-        int a,b;
+        int b;
         double dist;
 
-        public Node(int a,int b, double dist){
-            this.a=a;
+        public Node(int b, double dist){
             this.b=b;
             this.dist=dist;
         }
@@ -43,11 +42,11 @@ public class Main {
         n =  Integer.parseInt(br.readLine());
 
         ArrayList<Star> star = new ArrayList<>();
-        arr = new ArrayList<>();
-        parent = new int[n];
-        rank = new int[n];
+        arr = new ArrayList[n];
+        for(int i=0;i<n;i++){
+            arr[i] = new ArrayList<>();
+        }
 
-        for(int i=0;i<n;i++) parent[i] = i;
 
         for(int i=0;i<n;i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
@@ -59,28 +58,12 @@ public class Main {
 
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
-                arr.add(new Node(i,j,cal_dist(star.get(i).x, star.get(i).y , star.get(j).x,star.get(j).y)));
+                arr[i].add(new Node(j,cal_dist(star.get(i).x, star.get(i).y, star.get(j).x,star.get(j).y)));
+                arr[j].add(new Node(i,cal_dist(star.get(i).x, star.get(i).y, star.get(j).x,star.get(j).y)));
             }
         }
 
-        Collections.sort(arr);
-
-        double sum=0;
-        int cnt = 0;
-
-        for(int i=0;i<arr.size();i++){
-
-            Node node = arr.get(i);
-            if(find(node.a) != find(node.b)){
-                union(node.a,node.b);
-                sum+= node.dist;
-                cnt++;
-                if(cnt==n) break;
-
-            }
-
-        }
-        System.out.printf("%.2f\n",sum);
+        prim(0);
 
     }
 
@@ -89,30 +72,25 @@ public class Main {
         return Math.sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
     }
 
-    public static void union(int x,int y){
-        int x_parent = find(x);
-        int y_parent = find(y);
 
-        if(x_parent == y_parent){
-            return;
-        }
+    public static void prim(int start){
+        visit = new boolean[n];
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.offer(new Node(start, 0));
 
-        if(rank[x_parent] < rank[y_parent]){
-            parent[x_parent] = y_parent;
-        }
-        else if(rank[x_parent] > rank[y_parent]){
-            parent[y_parent] = x_parent;
-        }
-        else{
-            parent[x_parent] = y_parent;
-            rank[y_parent]++;
-        }
+        double sum=0;
+        while(!pq.isEmpty()){
+            Node node = pq.poll();
+            if(visit[node.b]) continue;
 
+            visit[node.b] = true;
+            sum+= node.dist;
+
+            for(Node n:arr[node.b]){
+                pq.add(n);
+            }
+        }
+        System.out.printf("%.2f\n", sum);
     }
-    public static int find(int x){
-        if(parent[x] == x) return x;
-        return parent[x] = find(parent[x]);
-    }
-
 
 }

38782b2a30d6b559c0dab9edc801dff8c3e567f8
이규섭 <guseob0701@naver.com>
2026-03-15 22:53:43 +0900

    [Silver III] Title: N과 M (1), Time: 1748 ms, Memory: 75532 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,34 +1,33 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.StringTokenizer;
 
-public class Main {
+class Main{
     static int n,m;
     static int[] arr;
-    static int[] output;
+    static  int[] output;
     static boolean[] visit;
 
-    public static void main(String[] args) throws Exception {
-
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        visit = new boolean[n];
-
 
         arr = new int[n];
-        for(int i=1;i<=n;i++) arr[i-1] = i;
         output = new int[n];
-        Permutation(n,m,0);
+        visit = new boolean[n];
+
+        for(int i=1;i<=n;i++) arr[i-1] = i;
+
+        permutation(0);
 
     }
 
-    static void Permutation(int n,int m,int depth){
+    public static void permutation(int depth){
         if(depth == m){
-            for (int i=0;i<m;i++) System.out.print(output[i]+" ");
+            for(int i=0;i<m;i++) System.out.print(output[i] + " ");
             System.out.println();
             return;
         }
@@ -36,7 +35,7 @@ public class Main {
             if(!visit[i]){
                 visit[i] = true;
                 output[depth] = arr[i];
-                Permutation(n,m,depth+1);
+                permutation(depth+1);
                 visit[i] = false;
             }
         }

426721885323b1db605f8475af78925eadb7895e
이규섭 <guseob0701@naver.com>
2026-03-19 10:51:09 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 436 ms, Memory: 51324 KB -BaekjoonHub

--- 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.java
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class Main {
 
-    static int n, m;
+    static int n,m;
     static ArrayList<Node>[] arr;
     static int v1, v2;
     static int[] dist;
@@ -12,87 +12,86 @@ public class Main {
 
     static class Node implements Comparable<Node> {
         int end, weight;
-
         Node(int end, int weight) {
             this.end = end;
             this.weight = weight;
-        }
 
+        }
         @Override
         public int compareTo(Node o) {
-            return Integer.compare(this.weight, o.weight);
+            return Integer.compare(weight, o.weight);
         }
     }
 
+
     public static void main(String[] args) throws Exception {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//       StringTokenizer st = new StringTokenizer(br.readLine());
 
         n = Integer.parseInt(br.readLine());
         m = Integer.parseInt(br.readLine());
 
-        arr = new ArrayList[n + 1];
-        for (int i = 1; i <= n; i++) arr[i] = new ArrayList<>();
+        arr = new ArrayList[n+1];
+        for(int i=1;i<=n;i++) arr[i] =new ArrayList<>();
 
-        for (int i = 0; i < m; i++) {
+        for(int i=0;i<m;i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             int w = Integer.parseInt(st.nextToken());
 
-            arr[a].add(new Node(b, w));
+            arr[a].add(new Node(b,w));
         }
 
-        map = new int[n + 1];
+        map = new int[n+1];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         v1 = Integer.parseInt(st.nextToken());
         v2 = Integer.parseInt(st.nextToken());
-
         dijkstra(v1);
-
         System.out.println(dist[v2]);
 
+        int cnt=1;
+        int k=v2;
         ArrayList<Integer> tmp = new ArrayList<>();
-        int k = v2;
-
-        while (k != 0) {
-            tmp.add(k);
-            if (k == v1) break;
+        tmp.add(k);
+        while (k != 0){
+            if(k == v1) break;
             k = map[k];
+            cnt++;
+            tmp.add(k);
         }
+        System.out.println(cnt);
+        k = v2;
 
-        System.out.println(tmp.size());
-
-        for (int i = tmp.size() - 1; i >= 0; i--) {
+        for(int i=tmp.size()-1;i>=0;i--){
             System.out.print(tmp.get(i) + " ");
         }
         System.out.println();
     }
 
-    public static void dijkstra(int start) {
-        dist = new int[n + 1];
-        Arrays.fill(dist, MAX);
-        Arrays.fill(map, 0);
 
-        dist[start] = 0;
+    public static void dijkstra(int start){
+        dist = new int[n+1];
+        for(int i=1;i<=n;i++) dist[i]=MAX;
+        dist[start]=0;
         PriorityQueue<Node> pq = new PriorityQueue<>();
-        pq.add(new Node(start, 0));
+        pq.add(new Node(start,0));
 
-        while (!pq.isEmpty()) {
+        while(!pq.isEmpty()){
             Node cur = pq.poll();
-
             if (cur.weight > dist[cur.end]) continue;
 
-            for (Node node : arr[cur.end]) {
-                int nextDist = dist[cur.end] + node.weight;
-
-                if (dist[node.end] > nextDist) {
-                    dist[node.end] = nextDist;
+            for(Node node:arr[cur.end]){
+                if(dist[node.end] >dist[cur.end] + node.weight){
+                    dist[node.end] = dist[cur.end] + node.weight;
+                    pq.add(new Node(node.end,dist[node.end]));
                     map[node.end] = cur.end;
-                    pq.add(new Node(node.end, nextDist));
                 }
             }
         }
+
     }
+
 }

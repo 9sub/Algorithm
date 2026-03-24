38b7053a14b2dfd044fb97857c9394e4f8195eb9
이규섭 <guseob0701@naver.com>
2026-03-24 12:19:53 +0900

    [Gold III] Title: 우주신과의 교감, Time: 552 ms, Memory: 41740 KB -BaekjoonHub

--- 백준/Gold/1774. 우주신과의 교감/우주신과의 교감.java
+++ 백준/Gold/1774. 우주신과의 교감/우주신과의 교감.java
@@ -1,116 +1,100 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Deque;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Main {
-
-    static int n,m;
-    static Edge[] points;
-    static ArrayList<Edge> connect;
-    static ArrayList<Node> real;
-    static int[] parent;
-    static int[] rank;
-
-    static class Edge{
-        int x;
-        int y;
-        Edge(int x,int y){
-            this.x=x;
-            this.y=y;
-        }
-    }
-
-    static class Node implements Comparable<Node>{
-        int start;
-        int end;
-        double distance;
-        Node (int start, int end, double distance){
-            this.start=start;
-            this.end=end;
-            this.distance=distance;
-        }
-        @Override
-        public int compareTo(Node o) {
-            return Double.compare(distance,o.distance);
-        }
-
-    }
-
+	
+	static int[] parents;
+	
+	static class Edge implements Comparable<Edge>{
+		int s;
+		int e;
+		double w;
+		public Edge(int s, int e, double w) {
+			this.s=s;
+			this.e=e;
+			this.w=w;
+		}
+		
+		public int compareTo(Edge o) {
+			return Double.compare(this.w, o.w);
+		}
+	}
+	
+	static int n, m;
+	
     public static void main(String[] args) throws Exception {
-
+    	
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
+    	StringTokenizer st = new StringTokenizer(br.readLine());
+        
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-
-        points = new Edge[n+1];
-        connect = new ArrayList<>();
-        real = new ArrayList<>();
-
-        parent = new int[n+1];
-        for(int i=0;i<=n;i++) parent[i]=i;
-        rank = new int[n+1];
-
-        for(int i=1;i<=n;i++){
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            points[i] = (new Edge(a,b));
+        
+        double[][] city = new double[n][2];
+    	parents = new int[n+1];
+        Arrays.fill(parents, -1);
+       
+        for(int i=0; i < n; i++) {
+        	st = new StringTokenizer(br.readLine());
+        	city[i][0] = Double.parseDouble(st.nextToken());
+        	city[i][1] = Double.parseDouble(st.nextToken());
         }
 
-        for(int i=0;i<m;i++){
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            union(a,b);
+        int count=0;
+        
+        for(int i=0; i < m; i++) {
+        	st = new StringTokenizer(br.readLine());
+        	int u = Integer.parseInt(st.nextToken())-1;
+        	int v = Integer.parseInt(st.nextToken())-1;
+        	if(union(u,v)) {
+            	count++;        		
+        	}
         }
+    	        
+    	ArrayList<Edge> edgeList = new ArrayList<>();
+    	
+    	for(int i=0; i < n; i++) {
+    		for(int j=i+1; j < n; j++) {
+    			double dist = Math.sqrt(Math.pow(city[i][0] - city[j][0],2) + Math.pow(city[i][1] - city[j][1], 2));
+    			edgeList.add(new Edge(i,j,dist));
+    		}
+    	}        	
 
-        for(int i=1;i<=n;i++){
-            for(int j=i+1;j<=n;j++){
-                real.add(new Node(i,j,cal(points[i].x, points[i].y,points[j].x,points[j].y)));
-
-            }
+    	 
+        Collections.sort(edgeList);
+        
+        double result=0;
+        for(Edge edge : edgeList) {
+        	if(union(edge.s, edge.e)) {
+        		result += edge.w;
+        		count++;
+        	}
+        	if(count==n-1) break;
         }
-
-        Collections.sort(real);
-
-        double sum=0;
-        for(int i=0;i<real.size();i++){
-            Node node = real.get(i);
-            if(find(node.start)!= find(node.end)){
-                union(node.start,node.end);
-                sum+= node.distance;
-
-            }
-        }
-
-        System.out.printf("%.2f\n", sum);
-
-
+        System.out.printf("%.2f", result);
     }
 
-    public static void union(int a, int b){
-        int parent_a = find(a);
-        int parent_b = find(b);
+	private static boolean union(int x, int y) {
+		int xroot = find(x);
+		int yroot = find(y);
+		if(xroot!=yroot) {
+			parents[yroot] = xroot;
+			return true;
+		}
+		return false;
+	}
 
-        if(rank[parent_a] > rank[parent_b]){
-            parent[parent_b] = parent_a;
-        }else if(rank[parent_a] < rank[parent_b]){
-            parent[parent_a] = parent_b;
-        }else{
-            parent[parent_b] = parent_a;
-            rank[parent_a]++;
-        }
-
-    }
-
-    public static int find(int x){
-        if(parent[x] == x) return x;
-        return parent[x] = find(parent[x]);
-    }
-
-    public static double cal(int x1, int y1, int x2, int y2){
-        return Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
-    }
-
+	private static int find(int x) {
+		if(parents[x] < 0) return x;
+		return parents[x] = find(parents[x]);
+	}
 }

74ae7dd51ffb696837167d01297174934673b6b5
이규섭 <guseob0701@naver.com>
2026-03-29 19:42:39 +0900

    [Platinum V] Title: 고층 빌딩, Time: 104 ms, Memory: 20340 KB -BaekjoonHub

---
+++ 백준/Platinum/1328. 고층 빌딩/고층 빌딩.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int n,l,r;
+    static long[][][] dp;
+    static long mod = 1000000007;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        l = Integer.parseInt(st.nextToken());
+        r = Integer.parseInt(st.nextToken());
+
+        dp = new long[n+1][l+1][r+1];
+
+        dp[1][1][1] = 1;
+
+        for(int i=2;i<=n;i++){
+            for(int j=1;j<=l;j++){
+                for(int k=1;k<=r;k++){
+                    dp[i][j][k] = (dp[i-1][j-1][k] + dp[i-1][j][k-1] + (i-2)*dp[i-1][j][k]) % mod;
+                }
+            }
+        }
+
+        System.out.println(dp[n][l][r]);
+    }
+
+
+}

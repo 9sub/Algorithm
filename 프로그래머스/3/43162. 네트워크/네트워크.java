442a7cbcb7a468c96ed911f4e49d87dda344e29a
이규섭 <guseob0701@naver.com>
2026-04-07 15:39:08 +0900

    [level 3] Title: 네트워크, Time: 0.73 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,31 +1,57 @@
+import java.util.*;
+
 class Solution {
     
     static boolean[] visit;
+    static int[] parent;
+    static int[] rank;
     
     public int solution(int n, int[][] computers) {
         int answer = 0;
         
-        visit = new boolean[n];
+        parent = new int[n];
+        rank = new int[n];
+        for(int i=0;i<n;i++)
+            parent[i] = i;
         
         for(int i=0;i<n;i++){
-            if(!visit[i]){
-                answer++;
-                dfs(computers, i);
+            for(int j=0;j<n;j++){
+                if(i != j && computers[i][j] == 1)
+                    union(i,j);
             }
         }
         
-        return answer;
+        Set<Integer> set = new HashSet<>();
+        for(int i=0;i<n;i++){
+            set.add(find(i));
+        }
+        System.out.println(set.toString());
+    
+        return set.size();
+        
     }
     
-    public void dfs(int[][] arr, int x){
-        visit[x] = true;
+    public int find(int x){
+        if(x == parent[x])
+            return x;
+        return parent[x]= find(parent[x]);
+    }
+    
+    public void union(int x, int y){
         
-        for(int i=0;i<arr.length;i++){
-            if(!visit[i] && arr[x][i] == 1){
-                dfs(arr, i);
-            }
+        int parent_x = find(x);
+        int parent_y = find(y);
+        
+        if(rank[parent_x] > rank[parent_y]){
+            parent[parent_y] = parent_x;
+        }else if(rank[parent_x]<rank[parent_y])
+            parent[parent_x] = parent_y;
+        else{
+            parent[parent_x] = parent_y;
+            rank[parent_y]++;
         }
         
     }
+        
     
 }

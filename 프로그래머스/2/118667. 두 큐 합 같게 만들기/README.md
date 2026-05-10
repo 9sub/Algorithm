bcb58a051751a643f6274daf6cc1722a1b27a5cd
이규섭 <guseob0701@naver.com>
2026-05-10 21:38:43 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 89.78 ms, Memory: 137 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/README.md
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 두 큐 합 같게 만들기 - 118667 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/118667) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/118667?language=java) 
 
 ### 성능 요약
 
-메모리: 18.4 MB, 시간: 147.96 ms
+메모리: 137 MB, 시간: 89.78 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 09일 14:41:22
+2026년 05월 10일 21:38:32
 
 ### 문제 설명
 
---
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] queue1, int[] queue2) {
+        long sum1=0;
+        long sum2=0;
+        int cnt=0;
+
+
+        Queue<Integer> que1 = new LinkedList<>();
+        Queue<Integer> que2 = new LinkedList<>();
+
+
+
+        for(int q : queue1) {
+            sum1 += q;
+            que1.offer(q);
+        }
+        for(int q : queue2) {
+            sum2+= q;
+            que2.offer(q);
+        }
+        if((sum1+sum2)%2 == 1) return -1;
+        
+        
+        long target = (sum1+sum2)/2;
+
+        while(cnt <= queue1.length * 4){
+            if(sum1 == target) return cnt;
+
+            if(sum1>target){
+                int tmp = que1.poll();
+                sum1 -= tmp;
+                que2.offer(tmp);
+            }else{
+                int tmp = que2.poll();
+                sum1 += tmp;
+                que1.offer(tmp);
+            }
+            cnt++;
+        }
+        return -1;
+    }
+}

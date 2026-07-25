02dc18a0802545c6b422f86247e5a7927fefcf17
이규섭 <guseob0701@naver.com>
2026-07-25 22:20:49 +0900

    [level 3] Title: 네트워크, Time: 4.14 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,57 +1,56 @@
+
 import java.util.*;
 
 class Solution {
     
-    static boolean[] visit;
+    static ArrayList<Integer>[] arr;
     static int[] parent;
-    static int[] rank;
     
     public int solution(int n, int[][] computers) {
         int answer = 0;
         
+        arr = new ArrayList[n+1];
+        for(int i=0;i<n;i++){
+            arr[i] = new ArrayList<>();
+        }
+        
         parent = new int[n];
-        rank = new int[n];
-        for(int i=0;i<n;i++)
-            parent[i] = i;
+        for(int i=0;i<n;i++) parent[i] = i;
         
         for(int i=0;i<n;i++){
+            int[] tmp = computers[i];
             for(int j=0;j<n;j++){
-                if(i != j && computers[i][j] == 1)
-                    union(i,j);
+                if(i==j) continue;
+                arr[i].add(computers[i][j]);
             }
         }
         
-        Set<Integer> set = new HashSet<>();
         for(int i=0;i<n;i++){
-            set.add(find(i));
-        }
-        System.out.println(set.toString());
-    
-        return set.size();
-        
-    }
-    
-    public int find(int x){
-        if(x == parent[x])
-            return x;
-        return parent[x]= find(parent[x]);
-    }
-    
-    public void union(int x, int y){
-        
-        int parent_x = find(x);
-        int parent_y = find(y);
-        
-        if(rank[parent_x] > rank[parent_y]){
-            parent[parent_y] = parent_x;
-        }else if(rank[parent_x]<rank[parent_y])
-            parent[parent_x] = parent_y;
-        else{
-            parent[parent_x] = parent_y;
-            rank[parent_y]++;
+            for(int j=0;j<n;j++){
+                if(i==j) continue;
+                if(computers[i][j] == 1){
+                    union(i,j);
+                }
+            }
         }
         
-    }
+        for(int i=0;i<n;i++){
+            if(i==parent[i]) {
+                answer++;
+            }
+        }
         
+        return answer;
+    }
+    
+    public static void union(int a,int  b){
+        a = find(a);
+        b = find(b);
+        if(a!=b)parent[a] =b;
+    }
+    public static int find(int a){
+        if(parent[a] == a) return a;
+        return parent[a] = find(parent[a]);
+    }
     
 }
